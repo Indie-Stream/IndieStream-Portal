052a2ce2853b0a2cf4a9147ff9c472d67a2361ae
sukhchainn <32098299+sukhchainn@users.noreply.github.com>
2020-07-29 10:13:50 +0530

    Initial commit after making my decently working frontend.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './subdivisions/principles/header/header.component';
+import { FooterComponent } from './subdivisions/principles/footer/footer.component';
+import { MainComponent } from './subdivisions/principles/main/main.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
